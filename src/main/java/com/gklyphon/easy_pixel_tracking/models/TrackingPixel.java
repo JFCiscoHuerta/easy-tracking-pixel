9e93312942e02c45a4244908c6f1e3d8008002b0
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-03-25 21:49:14 -0600

    Add Tracking & Auditable entities

---
+++ src/main/java/com/gklyphon/easy_pixel_tracking/models/TrackingPixel.java
@@ -0,0 +1,20 @@
+package com.gklyphon.easy_pixel_tracking.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tracking_pixel")
+public class TrackingPixel extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ip;
+    private String userAgent;
+    private String referer;
+}
