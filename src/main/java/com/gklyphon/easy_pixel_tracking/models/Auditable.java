3830327dd31be4c124ebc51c6b428b2072e2cee1
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-03-26 01:49:10 -0600

    Add Getters, Setters & Remove Lombok Annotations

--- src/main/java/com/gklyphon/easy_pixel_tracking/models/Auditable.java
+++ src/main/java/com/gklyphon/easy_pixel_tracking/models/Auditable.java
@@ -4,13 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @MappedSuperclass
 public class Auditable {
 
@@ -29,4 +25,19 @@ public class Auditable {
         this.updatedAt = LocalDateTime.now();
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
