657e5bddc4ced040973aacc340da545cef7d4c56
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-03-25 19:51:09 -0600

    Project Setup

---
+++ src/main/java/com/gklyphon/easy_pixel_tracking/EasyPixelTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.gklyphon.easy_pixel_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPixelTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPixelTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gklyphon/easy_pixel_tracking/EasyPixelTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gklyphon.easy_pixel_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPixelTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
