abd148fafbcbb959266fab9a7298de8d2b5c7cba
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2025-03-25 23:18:55 -0600

    Add TrackingPixel Service Interface & Implementation

---
+++ src/main/java/com/gklyphon/easy_pixel_tracking/services/impl/TrackingPixelServiceImpl.java
@@ -0,0 +1,91 @@
+package com.gklyphon.easy_pixel_tracking.services.impl;
+
+import com.gklyphon.easy_pixel_tracking.models.TrackingPixel;
+import com.gklyphon.easy_pixel_tracking.repository.ITrackingPixelRepository;
+import com.gklyphon.easy_pixel_tracking.services.ITrackingPixelService;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TrackingPixelServiceImpl implements ITrackingPixelService {
+
+    private final ITrackingPixelRepository trackingPixelRepository;
+
+    public TrackingPixelServiceImpl(ITrackingPixelRepository trackingPixelRepository) {
+        this.trackingPixelRepository = trackingPixelRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TrackingPixel> findAll(Pageable pageable) {
+        return trackingPixelRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional
+    public Page<TrackingPixel> findAllByOrderByCreatedAt(Pageable pageable) {
+        return trackingPixelRepository.findAllByOrderByCreatedAt(pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TrackingPixel> findByIpOrderByCreatedAt(String ip, Pageable pageable) {
+        return trackingPixelRepository.findByIpOrderByCreatedAt(ip, pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TrackingPixel> findByUserAgentByCreatedAt(String userAgent, Pageable pageable) {
+        return trackingPixelRepository.findByUserAgentByCreatedAt(userAgent, pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<TrackingPixel> findByRefererByCreatedAt(String referer, Pageable pageable) {
+        return trackingPixelRepository.findByRefererByCreatedAt(referer, pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public TrackingPixel findById(Long id) {
+        return trackingPixelRepository.findById(id)
+                .orElseThrow();
+    }
+
+    @Override
+    @Transactional
+    public TrackingPixel save(TrackingPixel trackingPixel) {
+        try {
+            return trackingPixelRepository.save(trackingPixel);
+        } catch (Exception ex) {
+            throw new ServiceException("Unexpected error while saving.", ex);
+        }
+    }
+
+    @Override
+    @Transactional
+    public TrackingPixel update(Long id, TrackingPixel trackingPixel) {
+        TrackingPixel originalTrackingPixel = findById(id);
+        try {
+            BeanUtils.copyProperties(trackingPixel, originalTrackingPixel, "id", "createdAt", "updatedAt");
+            return trackingPixelRepository.save(originalTrackingPixel);
+        } catch (Exception ex) {
+            throw new ServiceException("Unexpected error while updating.", ex);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Long id) {
+        findById(id);
+        try {
+            trackingPixelRepository.deleteById(id);
+        } catch (Exception ex) {
+            throw new ServiceException("Unexpected error while deleting.", ex);
+        }
+    }
+}
